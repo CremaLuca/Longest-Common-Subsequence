30be1413776aa7f9661b1784552fef3e5787cee2
Marco Mariotto <56561006+d-u-d-e@users.noreply.github.com>
2021-07-14 05:17:02 -0700

    fixed main.cpp

--- LCS_parallel/main.cpp
+++ LCS_parallel/main.cpp
@@ -1,5 +1,6 @@
 #include <mpi.h>
 #include <algorithm>
+#include <cmath>
 
 using namespace std;
 
@@ -9,16 +10,42 @@ int N, M, P;
  * @param d index of the diagonal.
  * @returns Length of the given diagonal.
  */
-int get_diag_len(int d, int M, int N){
+int diag_length(int d){
     int arr[] = {d+1, M, N, M+N-1-d};
     return *min_element(arr, arr + 3);
 }
 
 /**
+ * @param d index of the diagonal.
+ * @param i index of the processor
+ * @returns a pair (s, e) indicating the starting/ending cell on
+ * the diagonal d assigned to processor i. index e is exclusive,
+ * while index s in inclusive.
+ */
+pair<int, int> diag_start_end(int d, int i){
+    int len = diag_length(d);
+    int p = min(P, len);
+    int ceil_size = ceil((float)len/p);
+    int floor_size = floor((float)len/p);
+    int rem = len % p;
+    int start, end;
+    if (i < rem){
+        start = i * ceil_size;
+        end = start + ceil_size;
+    }
+    else{
+        start = rem * ceil_size + (i-rem) * floor_size;
+        end = start + floor_size;
+    }
+    return pair(start, end);
+}
+
+
+/**
  * @param c coordinates of a matrix cell
  * @returns Index of the diagonal where the cell belongs
  */
-int cell_2_diag(pair<int, int> c){
+int cell_diag(pair<int, int> c){
     return c.first + c.second;
 }
 
@@ -26,14 +53,48 @@ int cell_2_diag(pair<int, int> c){
  * @param c coordinates of a matrix cell
  * @returns The index of the cell w.r.t. the diagonal it belongs to.
  */
-int cell_diag_index(pair<int, int> c, int N){
+int cell_diag_index(pair<int, int> c){
     return min(c.first, N-c.second-1);
 }
+
+
+int cell_proc(pair<int, int> c){
+    int d = cell_diag(c);
+    int len = diag_length(d);
+    int pos = cell_diag_index(c);
+    int p = min(len, P);
+    int ceil_size = ceil((float)len/p);
+    int floor_size = floor((float)len/p);
+    int rem = len % p;
+    if (pos < ceil_size * rem)
+        return floor((float) pos / ceil_size);
+    else
+        return floor((float)(pos - rem) / floor_size);
+}
+
+vector<pair<int, int>> matrix_elements(int i){
+
+    vector<pair<int, int>> v;
+
+    if(i >= min(M, N))
+        return v;
+
+    for(int d = i; d < N+M-1-i; d++){
+        pair<int, int> indices = diag_start_end(d, i);
+        for(int e = indices.first; e < indices.second; e++){
+            int x = max(0, d-N+1) + e;
+            int y = min(d, N-1) - e;
+            v.push_back(pair(x, y));
+        }
+    }
+    return v;
+}
+
+
 
 int main()
 {
     int rank, P;
-
     char X[] = {'A', 'B', 'C', 'B', 'D', 'A', 'B'};
     char Y[] = {'B', 'D', 'C', 'A', 'B', 'A'};
     // Extra: "Currently, MPI_Init takes two arguments that are not necessary, and the extra parameters
