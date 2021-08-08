1b2cc5cc3220a2f10ae5d416ae6e93310d17e81a
Luca Crema <lc.crema@hotmail.com>
2021-08-08 18:56:23 +0200

    LCS reconstruction working properly

--- LCS_parallel/main.cpp
+++ LCS_parallel/main.cpp
@@ -13,7 +13,8 @@ using namespace std;
 typedef pair<int, int> cell;
 
 const int COMPUTE_LCS_TAG = 1;
-const int STOP_TAG = -1;
+const int STORE_LCS_TAG = 2;
+const int STOP_TAG = 3;
 
 int N, M, P;
 
@@ -78,6 +79,9 @@ int cell_diag_index(cell c){
  * @returns The index of the processor responsible for it.
  */
 int cell_proc(cell c){
+    if(c.first < 0 || c.second < 0){
+        return -1;
+    }
     int d = cell_diag(c);
     int len = diag_length(d);
     int pos = cell_diag_index(c);
@@ -128,14 +132,14 @@ int main()
 {
     int rank;
 
-    char X[] = {'A', 'B', 'C', 'B', 'D', 'A', 'B'};
-    char Y[] = {'B', 'D', 'C', 'A', 'B', 'A'};
+    char X[] = {'B', 'D', 'C', 'A', 'B', 'A'};
+    char Y[] = {'A', 'B', 'C', 'B', 'D', 'A', 'B'};
     // Compute the size of X and Y
 
-    N = sizeof(X)/sizeof(X[0]);
-    M = sizeof(Y)/sizeof(Y[0]);
+    M = sizeof(X)/sizeof(X[0]);
+    N = sizeof(Y)/sizeof(Y[0]);
 
-    printf("N (size of X) is %d and M (size of Y) is %d\n", N, M);
+    printf("N (size of Y) is %d and M (size of X) is %d\n", N, M);
 
     // Extra: "Currently, MPI_Init takes two arguments that are not necessary, and the extra parameters
     //         are simply left as extra space in case future implementations might need them"
@@ -162,7 +166,7 @@ int main()
     }
 
     // Print indices_str to console
-    printf("%s", indices_str.c_str());
+    printf("%s\n", indices_str.c_str());
 
 
     //********************************
@@ -212,11 +216,6 @@ int main()
             up_left_value = local_memory[up_left];
         }
 
-        printf("p%d is working on (%d, %d) with up (%d, %d): %d,"
-        " left (%d, %d): %d,"
-        "up_left (%d, %d): %d\n", rank, current_cell.first, current_cell.second, up.first, up.second,
-               up_value, left.first, left.second, left_value, up_left.first, up_left.second, up_left_value);
-
         // Compute the value of the current cell c
         int c_value = 0;
         if (X[current_cell.first] == Y[current_cell.second]){
@@ -225,7 +224,12 @@ int main()
             c_value = max(up_value, left_value);
         }
 
-        printf("p%d: c_value is %d\n", rank, c_value);
+        printf("p%d is working on %c %c (%d, %d) = %d, with up (%d, %d): %d,"
+               " left (%d, %d): %d,"
+               "up_left (%d, %d): %d\n",
+               rank, X[current_cell.first], Y[current_cell.second], current_cell.first, current_cell.second, c_value, up.first, up.second,
+               up_value, left.first, left.second, left_value, up_left.first, up_left.second, up_left_value);
+
         // Store the value in local memory
         local_memory[current_cell] = c_value;
         // Send the value to the next processors
@@ -279,15 +283,15 @@ int main()
         if(begin_reconstruction){
             // Start the LCS reconstruction
 
-            while(i >= 0 && j >= 0){
+            while(i >= -1 && j >= -1){
 
-                if(i == 0 || j == 0){ //LCS DONE
+                if(i == -1 || j == -1){ //LCS DONE
+                    // Send stop signal to all other procesors
                     for(int i = 0; i < P; i++)
                         if(i != rank)
                             MPI_Isend(nullptr, 0, MPI_CHAR, i, STOP_TAG, MPI_COMM_WORLD, &send_req);
 
                     //TODO PRINT THE LCS AND QUIT
-
                     goto done;
                 }
 
@@ -297,9 +301,10 @@ int main()
                 cell up_left = cell(i-1, j-1);
                 int current_value = local_memory[current];
                 int up_value = local_memory[up];
-                int up_left_value = local_memory[up_left];
-                if(current_value == up_left_value + 1){
-                    lcs = X[i-1] + lcs;
+                printf("p%d: cv: %d, up: %d, i: %d, j: %d, X[i]: %c, Y[j]: %c\n", rank, current_value, up_value, i, j, X[i], Y[j]);
+                if(X[i] == Y[j]){
+                    printf("p%d: appending char %c\n", rank, X[i]);
+                    lcs = X[i] + lcs;
                     i--; j--;
                 }else if (current_value == up_value){
                     i--;
@@ -309,13 +314,20 @@ int main()
 
                 cell next = cell(i, j);
                 int next_proc = cell_proc(next);
-                // Send the lcs to the next processor so that it can continue
+                // Send the coordinates to the next processor so that it can continue and the lcs to p0
                 if(next_proc != rank){
-                    printf("p%d: sending %s to p%d\n", rank, lcs.c_str(), next_proc);
-                    MPI_Isend(lcs.c_str(), lcs.size(), MPI_CHAR, next_proc, COMPUTE_LCS_TAG, MPI_COMM_WORLD, &send_req);
-                    //TODO SEND ALSO I, J
-                    begin_reconstruction = false;
-                    break;
+                    if(rank != 0){
+                        printf("p%d: sending %s to p0\n", rank, lcs.c_str());
+                        MPI_Isend(lcs.c_str(), lcs.size(), MPI_CHAR, 0, STORE_LCS_TAG, MPI_COMM_WORLD, &send_req);
+                    }else{
+                        printf("p0: computed new lcs %s\n", lcs.c_str());
+                    }
+                    if(next_proc != -1){
+                        printf("p%d: sending coords (%d, %d) to p%d\n", rank, i, j, next_proc);
+                        MPI_Isend(std::begin({i, j}), 2, MPI_INT, next_proc, COMPUTE_LCS_TAG, MPI_COMM_WORLD, &send_req);
+                        begin_reconstruction = false;
+                        break;
+                    }
                 }
             }
         }
@@ -328,11 +340,23 @@ int main()
             goto done;
         } else if (status.MPI_TAG == COMPUTE_LCS_TAG){
             // Read the message
+            int* lcs_coords = new int[2];
+            MPI_Recv(lcs_coords, 2, MPI_INT, status.MPI_SOURCE, status.MPI_TAG, MPI_COMM_WORLD, &status);
+            i = lcs_coords[0];
+            j = lcs_coords[1];
+            if(rank != 0)
+                lcs = "";
+            begin_reconstruction = true;
+            printf("p%d: computing lcs starting from (%d, %d)\n", rank, i, j);
+        } else if (status.MPI_TAG == STORE_LCS_TAG){ // Only processor 0 will receive this message tag
+            // Read the lcs
             int lcs_length;
             MPI_Get_count(&status, MPI_CHAR, &lcs_length);
-            char* lcs_buffer = new char[lcs_length];
+            char* lcs_buffer = new char[lcs_length+1];
+            lcs_buffer[lcs_length] = '\0';
             MPI_Recv(lcs_buffer, lcs_length, MPI_CHAR, status.MPI_SOURCE, status.MPI_TAG, MPI_COMM_WORLD, &status);
-            begin_reconstruction = true;
+            printf("p0: received %d character of lcs %s\n", lcs_length, lcs_buffer);
+            lcs = lcs_buffer + lcs;
         }
     }
 
@@ -343,6 +367,10 @@ int main()
     //********************************
 
 done:
+    MPI_Barrier(MPI_COMM_WORLD);
+    if(rank == 0){
+        printf("p0: reconstructed lcs string %s\n", lcs.c_str());
+    }
     MPI_Finalize();
     return 0;
 }
